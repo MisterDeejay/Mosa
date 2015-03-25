43e92e190f2aaeb6d6d45f27d88f687c69354674
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-03-25 11:50:28 -0700

    serve static assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module MosaApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
   end
