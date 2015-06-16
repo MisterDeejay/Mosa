d21b33e11e29afa69b0264a6fd419d4465ea971c
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-16 11:48:34 -0700

    updates to restaurant show page

--- app/assets/javascripts/models/restaurant.js
+++ app/assets/javascripts/models/restaurant.js
@@ -60,16 +60,15 @@ window.Mosa.Models.Restaurant = Backbone.Model.extend({
 
 	distance: function(){
 		if(window.latLng) {
-			currLoc = new google.maps.LatLng(window.latLng[0],
+			var currLoc = new google.maps.LatLng(window.latLng[0],
 				window.latLng[1]);
-			restLoc = new google.maps.LatLng(this.get("lat"),
+			var restLoc = new google.maps.LatLng(this.get("lat"),
 				this.get("lng"));
 
 
-			dist = google.maps.geometry.spherical.computeDistanceBetween(
+			var dist = google.maps.geometry.spherical.computeDistanceBetween(
 				currLoc, restLoc
 			);
-
 			return dist;
 		} else {
 			return 0;
